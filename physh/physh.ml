576120cbe3380f8f925e986fbf5ea9a499bc2007
Frédéric Bour <frederic.bour@lakaban.net>
2026-07-31 22:02:21 +0900

    physh: re-enable, fix incorrect primitive names (>_<)

--- physh/physh.ml
+++ physh/physh.ml
@@ -3,27 +3,27 @@ let null = ref ()
 module Set = struct
   type 'a t
 
-  external phys_set_alloc : 'a array -> unit ref -> 'a t = "ml_phys_set_alloc"
-  external phys_set_add : 'a t -> 'a -> unit ref-> unit = "ml_phys_set_add"
-  external phys_set_mem : 'a t -> 'a -> unit ref-> bool = "ml_phys_set_mem"
-  external phys_set_length : 'a t -> int = "ml_phys_set_length"
+  external physh_set_alloc : 'a array -> unit ref -> 'a t = "ml_physh_set_alloc"
+  external physh_set_add : 'a t -> 'a -> unit ref-> unit = "ml_physh_set_add"
+  external physh_set_mem : 'a t -> 'a -> unit ref-> bool = "ml_physh_set_mem"
+  external physh_set_length : 'a t -> int = "ml_physh_set_length"
 
-  let create () = phys_set_alloc [||] null
-  let length = phys_set_length
-  let add t x = phys_set_add t x null
-  let mem t x = phys_set_mem t x null
+  let create () = physh_set_alloc [||] null
+  let length = physh_set_length
+  let add t x = physh_set_add t x null
+  let mem t x = physh_set_mem t x null
 end
 
 module Map = struct
   type ('a,'b) t
 
-  external phys_map_alloc : 'a array -> unit ref -> ('a, 'b) t = "ml_phys_map_alloc"
-  external phys_map_add  : ('a, 'b) t -> 'a -> 'b -> unit ref -> unit = "ml_phys_map_add"
-  external phys_map_find : ('a, 'b) t -> 'a -> unit ref -> 'b = "ml_phys_map_find"
-  external phys_map_length : ('a, 'b) t -> int = "ml_phys_map_length"
+  external physh_map_alloc : 'a array -> unit ref -> ('a, 'b) t = "ml_physh_map_alloc"
+  external physh_map_add  : ('a, 'b) t -> 'a -> 'b -> unit ref -> unit = "ml_physh_map_add"
+  external physh_map_find : ('a, 'b) t -> 'a -> unit ref -> 'b = "ml_physh_map_find"
+  external physh_map_length : ('a, 'b) t -> int = "ml_physh_map_length"
 
-  let create () = phys_map_alloc [||] null
-  let length = phys_map_length
-  let add t k v = phys_map_add t k v null
-  let find t k = phys_map_find t k null
+  let create () = physh_map_alloc [||] null
+  let length = physh_map_length
+  let add t k v = physh_map_add t k v null
+  let find t k = physh_map_find t k null
 end
